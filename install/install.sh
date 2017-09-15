7282beabef240422bdce0763be49c04a38160e1e
farsonic <fp@prowsehouse.com>
2017-09-15 10:52:49 +1000

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
+echo "__________.__          ________  .__                     ";
+echo "\______   \__|         \______ \ |__| ______ ____  ____  ";
+echo " |     ___/  |  ______  |    |  \|  |/  ___// ___\/  _ \ ";
+echo " |    |   |  | /_____/  |    \   \  |\___ \\  \__(  <_> )";
+echo " |____|   |__|         /_______  /__/____  >\___  >____/ ";
+echo "                               \/        \/     \/       ";
+
+
+
 #Confirm if user is either root or sudo root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
